60f07f2ac27034485b441fc0fd4e3fb06527efd0
Huang Shewei <huangshewei@gmail.com>
2022-02-15 14:42:03 -0500

    Index display all the tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,34 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+    # raise
+  end
+
+  def new
+    # Make a new task (show the form and fill it)
+    @task = Task.new
+    # raise
+  end
+
+  def create
+    # This is what happens when we hit "send"
+    @task = Task.new(task_params)
+    @task.save
+    # raise
+    redirect_to task_path(@task)
+  end
+
+  def show
+    # raise
+    @task = Task.find(params[:id])
+    # raise
+  end
+
+  private
+
+  # by default rails prevent you from creating a new task, we need strong params
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+    # params.require("task").permit("title", "details", "completed")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
 Rails.application.routes.draw do
   # Verb 'path', to: 'controller#action'
-  # Create
-  get '/tasks/new', to: 'tasks#new'
-  # Read
+
+  # Read all tasks
   get '/tasks', to: 'tasks#index'
-  # Update
-  get '/tasks/:id', to: 'tasks#edit'
-  # Delete
+
+  # Create a task
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+  post '/tasks', to: 'tasks#create'
+
+  # Read a task
+  get '/tasks/:id', to: 'tasks#show', as: :task
+
+  # Update a task
+  get '/tasks/:id', to: 'tasks#edit', as: :edit_task
+  patch '/tasks/:id', to: 'tasks#update'
+
+  # Delete a task
   delete '/tasks/:id', to: 'tasks#destroy'
 end
